97fddc78cc44c5e86ba357bb14a565eb82b54cde
yifan <iffy713@gmail.com>
2022-09-22 18:02:32 -0400

    Add User authentication Express middleware

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,9 +1,51 @@
 const router = require('express').Router();
+const { restoreUser } = require('../../utils/auth.js');
 
-router.post('/test', function(req,res){
-    res.json({
-        requestBody: req.body
-    })
-})
+// //Test User Auth Middlewares
+// // GET /api/set-token-cookie
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       }
+//     });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// });
 
-module.exports = router;
+
+
+// //Test User Auth Middlewares
+// // GET /api/restore-user
+
+
+// // GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+    //   '/require-auth',
+    //   requireAuth,
+//   (req, res) => {
+    //     return res.json(req.user);
+    //   }
+    // );
+    // router.get(
+        //   '/restore-user',
+        //   (req, res) => {
+            //     return res.json(req.user);
+            //   }
+            // );
+
+
+
+            router.post('/test', function(req,res){
+                res.json({
+                    requestBody: req.body
+                })
+            })
+
+
+
+            router.use(restoreUser);
+            module.exports = router;
