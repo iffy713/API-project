2f82657560d21cfaa6fd4148fdfc67df47dd2f39
yifan <iffy713@gmail.com>
2022-09-23 12:31:44 -0400

    Add User input validation on user signup backend endpoint

--- authenticate-me/backend/routes/api/users.js
+++ authenticate-me/backend/routes/api/users.js
@@ -5,20 +5,61 @@ const express = require('express');
 const { setTokenCookie, requireAuth } = require('../../utils/auth')
 const { User } = require('../../db/models')
 
+//Validating Signup Request Body
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation')
+
 const router = express.Router();
 
 //sign up
-router.post(
-    '/',
-    async(req, res) => {
-        const { email, password, username } = req.body
-        const user = await User.signup({ email, username, password })
+// router.post(
+//     '/',
+//     async(req, res) => {
+//         const { email, password, username } = req.body
+//         const user = await User.signup({ email, username, password })
 
-        await setTokenCookie(res, user)
+//         await setTokenCookie(res, user)
 
-        return res.json({ user })
-    }
-)
+//         return res.json({ user })
+//     }
+// )
+
+const validateSignup = [
+    check('email')
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage('Please provide a valid email.'),
+    check('username')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 4 })
+      .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+      .not()
+      .isEmail()
+      .withMessage('Username cannot be an email.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 6 })
+      .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+  ];
+
+router.post('/', validateSignup, async (req, res) => {
+    const { email, password, username } = req.body;
+    console.log("password",password)
+    const user = await User.signup({ email, username, password });
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user,
+    });
+  }
+);
+
+
+
+
 
 
 module.exports = router;
