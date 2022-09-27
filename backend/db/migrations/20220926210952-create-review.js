f1b7fa2d2842cb15d7613cb3d78ca77af05683ca
yifan <iffy713@gmail.com>
2022-09-26 21:10:48 -0400

    migrations and models

---
+++ backend/db/migrations/20220926210952-create-review.js
@@ -0,0 +1,50 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      soptId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Spots"
+        },
+        onDelete: "CASCADE",
+        allowNull: false
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+        },
+        onDelete: "CASCADE",
+        allowNull: false,
+      },
+      review: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      stars: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Reviews');
+  }
+};
