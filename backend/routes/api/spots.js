f88d487d2382eefab97beea0b70214002794116f
yifan <iffy713@gmail.com>
2022-09-28 12:44:06 -0400

    post new spot done

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,11 +2,14 @@ const express = require('express')
 
 const { check } = require('express-validator')
 const { handleValidationErrors } = require('../../utils/validation')
-const { Spot, Review, Booking, sequelize } = require('../../db/models')
+const { requireAuth } = require('../../utils/auth')
+const { Spot, Review, Booking, SpotImage,sequelize } = require('../../db/models')
+const user = require('../../db/models/user')
+
 
 const router = express.Router()
 
-const validateSport = [
+const validateSpot = [
     check('address')
       .exists({checkFalsy: true})
       .withMessage('Street address is required'),
@@ -39,10 +42,10 @@ const validateSport = [
 ]
 
 router.get('/', async(req,res)=>{
-
 //Get all Spots with avgRating & previewImage
     const spots = await Spot.findAll()
     for(let spot of spots ){
+        //avgRating added
         const spotReview = await spot.getReviews({
             attributes: [
                 [
@@ -52,11 +55,30 @@ router.get('/', async(req,res)=>{
         })
         console.log(spotReview[0])
         let avgRating = spotReview[0].dataValues.avgRating
-        spot.dataValues.avgRating = Number(avgRating).toFixed(2)
+        spot.dataValues.avgRating = Number(avgRating).toFixed(1)
+
+        //previewImage added
+        const spotImage = await SpotImage.findByPk(spot.id)
+        spot.dataValues.previewImage = spotImage.url
     }
-
-
     return res.json(spots)
+})
+
+router.post('/',  validateSpot, requireAuth, async(req,res)=>{
+    const { address, city, state, country, lat, lng, name, description, price } = req.body
+    const newSpot = await Spot.create({
+        ownerId: req.user.id,
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price
+    })
+    return res.json(newSpot)
 })
 
 module.exports = router
