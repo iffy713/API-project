024ffe95411b4a5e3a74e18ee675e76e03373730
yifan <iffy713@gmail.com>
2022-10-15 22:23:32 -0400

    reset the json file

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
-// router.get("/api/csrf/restore", (req, res)=>{
-//   const csrfToken = res.csrfToken()
-//   res.cookie("XSRF-TOKEN", csrfToken)
-//   res.status(200).json({
-//     "XSRF-Token": csrfToken
-//   })
-// })
-
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
